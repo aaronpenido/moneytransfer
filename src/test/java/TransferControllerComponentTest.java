505d57ccca17d111a6934e2bf0b9883cdace1b24
Aaron Penido <aaron.penido@sonicdrivein.com>
2018-10-15 16:10:19 -0300

    Add mockHttp calls

--- src/test/java/TransferControllerComponentTest.java
+++ src/test/java/TransferControllerComponentTest.java
@@ -1,11 +1,6 @@
 import com.google.gson.Gson;
 import controllers.TransferController;
 import models.Transfer;
-import org.jboss.resteasy.core.Dispatcher;
-import org.jboss.resteasy.core.SynchronousDispatcher;
-import org.jboss.resteasy.core.SynchronousExecutionContext;
-import org.jboss.resteasy.mock.MockDispatcherFactory;
-import org.jboss.resteasy.mock.MockHttpRequest;
 import org.jboss.resteasy.mock.MockHttpResponse;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,9 +10,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import requestbodies.TransferRequestBody;
 import services.TransferCreator;
 
-import javax.ws.rs.core.MediaType;
 import java.math.BigDecimal;
-import java.net.URISyntaxException;
 
 import static javax.ws.rs.core.Response.Status.CREATED;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,18 +20,18 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class TransferControllerComponentTest {
 
-    private static Dispatcher dispatcher;
-
     private TransferController transferController;
 
     @Mock
     private TransferCreator transferCreator;
 
+    private MockHttp mockHttp;
+
     @Before
     public void setUp() {
         transferController = new TransferController(transferCreator);
-        dispatcher = MockDispatcherFactory.createDispatcher();
-        dispatcher.getRegistry().addSingletonResource(transferController);
+
+        mockHttp = new MockHttp(transferController);
     }
 
     @Test
@@ -50,30 +43,8 @@ public class TransferControllerComponentTest {
 
         String requestBody = new Gson().toJson(transferRequestBody);
 
-        MockHttpResponse response = sendAsyncPostRequest("/transfers", requestBody);
+        MockHttpResponse response = mockHttp.sendAsyncPostRequest("/transfers", requestBody);
 
         assertThat(response.getStatus()).isEqualTo(CREATED.getStatusCode());
-    }
-
-    private MockHttpResponse sendAsyncPostRequest(String path, String requestBody) throws URISyntaxException {
-
-        MockHttpRequest request = MockHttpRequest.post(path);
-
-        if (requestBody != null) {
-            request.accept(MediaType.APPLICATION_JSON);
-            request.contentType(MediaType.APPLICATION_JSON_TYPE);
-            request.content(requestBody.getBytes());
-        }
-
-        MockHttpResponse response = new MockHttpResponse();
-        SynchronousExecutionContext synchronousExecutionContext = new SynchronousExecutionContext((SynchronousDispatcher) dispatcher, request, response);
-        request.setAsynchronousContext(synchronousExecutionContext);
-
-        return sendHttpRequest(request, response);
-    }
-
-    private MockHttpResponse sendHttpRequest(MockHttpRequest request, MockHttpResponse response) {
-        dispatcher.invoke(request, response);
-        return response;
     }
 }
